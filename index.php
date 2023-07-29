b45b0fc797e3c4868f7036bec8373d23d113db2a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-28 22:19:53 -0600

    Responde un json cuando se visita el endpoint /

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php 
 //redireccionar a la vista de login
-header ('Location: vistas/login.html');
-?>
+// header ('Location: vistas/login.html');
+
+require __DIR__ . '/vendor/autoload.php';
+
+app()->cors();
+
+app()->get('/', function () {
+  response()->json([
+    'message' => 'Hello World!'
+  ]);
+});
+
+app()->run();

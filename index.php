3fb6aad22a3dc078f04dff5a50b7e2077928b417
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-14 21:05:43 -0600

    Redirige al usuario a la home page

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/src/Routes/ProductRoutes.php';
 
-db()->connect('localhost', 'dbsistema_cv', 'root', 'devroot', 'mysql');
-
-app()->cors();
-
-app()->get('/', function () {
-  response()->json([
-    "Hello from PHP-MVC-Project"
-  ]);
-});
-
-app()->run();
+header("Location:public/index.html");

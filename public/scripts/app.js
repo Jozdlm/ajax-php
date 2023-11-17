22f9ecd0b3b44322ae7f8fa846ac7f1785e875ba
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-16 20:42:13 -0600

    Obtiene los datos de la API y los muestra en consola

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const API_URL = "http://localhost:3000/api/categories.php";
+const tableBody = document.getElementById("tbody");
+
+function main() {
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch();
+}
+
+main();

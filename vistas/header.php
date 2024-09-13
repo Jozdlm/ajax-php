d437bc7ad6e2cc31ea46d4bf42a47d502a38a859
Jozuan Lopez <jozuanlopez@gmail.com>
2024-09-13 08:41:00 -0600

    Updates the href path to the products page

--- vistas/header.php
+++ vistas/header.php
@@ -114,7 +114,7 @@ if (strlen(session_id()) < 1)
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li id="lArticulos"><a href="vistas/articulo.php"><i class="fa fa-circle-o"></i> Artículos</a></li>
+                <li id="lArticulos"><a href="app/products/"><i class="fa fa-circle-o"></i> Artículos</a></li>
                 <li id="lCategorias"><a href="app/categories/"><i class="fa fa-circle-o"></i> Categorías</a></li>
               </ul>
             </li>';

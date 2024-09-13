5fcaeccc81793537a947f6148ddab3f7371f167a
Jozuan Lopez <jozuanlopez@gmail.com>
2024-09-13 08:34:13 -0600

    Split in two pages the products page

--- app/products/index.php
+++ app/products/index.php
@@ -18,11 +18,11 @@ if (!isset($_SESSION["nombre"])) {
           <div class="col-md-12">
             <div class="box">
               <div class="box-header with-border">
-                <h1 class="box-title">Artículo <button class="btn btn-success" id="btnagregar"
-                    onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button> <a
-                    href="../reportes/rptarticulos.php" target="_blank"><button class="btn btn-info"><i
-                        class="fa fa-clipboard"></i> Reporte</button></a></h1>
+                <h1 class="box-title">Artículo</h1>
                 <div class="box-tools pull-right">
+                  <a class="btn btn-success" id="btnagregar" href="app/products/add-product.php"><i
+                      class="fa fa-plus-circle"></i> Agregar</a> <a href="/reportes/rptarticulos.php"
+                    target="_blank"><button class="btn btn-info"><i class="fa fa-clipboard"></i> Reporte</button></a>
                 </div>
               </div>
               <!-- /.box-header -->
@@ -51,53 +51,6 @@ if (!isset($_SESSION["nombre"])) {
                   </tfoot>
                 </table>
               </div>
-              <div class="panel-body" id="formularioregistros">
-                <form name="formulario" id="formulario" method="POST">
-                  <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <label>Nombre(*):</label>
-                    <input type="hidden" name="idarticulo" id="idarticulo">
-                    <input type="text" class="form-control" name="nombre" id="nombre" maxlength="100" placeholder="Nombre"
-                      required>
-                  </div>
-                  <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <label>Categoría(*):</label>
-                    <select id="idcategoria" name="idcategoria" class="form-control selectpicker" data-live-search="true"
-                      required></select>
-                  </div>
-                  <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <label>Stock(*):</label>
-                    <input type="number" class="form-control" name="stock" id="stock" required>
-                  </div>
-                  <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <label>Descripción:</label>
-                    <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="256"
-                      placeholder="Descripción">
-                  </div>
-                  <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <label>Imagen:</label>
-                    <input type="file" class="form-control" name="imagen" id="imagen"
-                      accept="image/x-png,image/gif,image/jpeg">
-                    <input type="hidden" name="imagenactual" id="imagenactual">
-                    <img src="" width="150px" height="120px" id="imagenmuestra">
-                  </div>
-                  <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <label>Código:</label>
-                    <input type="text" class="form-control" name="codigo" id="codigo" placeholder="Código Barras">
-                    <button class="btn btn-success" type="button" onclick="generarbarcode()">Generar</button>
-                    <button class="btn btn-info" type="button" onclick="imprimir()">Imprimir</button>
-                    <div id="print">
-                      <svg id="barcode"></svg>
-                    </div>
-                  </div>
-                  <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i>
-                      Guardar</button>
-
-                    <button class="btn btn-danger" onclick="cancelarform()" type="button"><i
-                        class="fa fa-arrow-circle-left"></i> Cancelar</button>
-                  </div>
-                </form>
-              </div>
               <!--Fin centro -->
             </div><!-- /.box -->
           </div><!-- /.col -->
@@ -108,9 +61,9 @@ if (!isset($_SESSION["nombre"])) {
     <!--Fin-Contenido-->
     <?php
   } else {
-     require $_SERVER['DOCUMENT_ROOT'] . '/vistas/noacceso.php';
+    require $_SERVER['DOCUMENT_ROOT'] . '/vistas/noacceso.php';
   }
-   require $_SERVER['DOCUMENT_ROOT'] . '/vistas/footer.php';
+  require $_SERVER['DOCUMENT_ROOT'] . '/vistas/footer.php';
   ?>
   <script type="text/javascript" src="/public/js/JsBarcode.all.min.js"></script>
   <script type="text/javascript" src="/public/js/jquery.PrintArea.js"></script>
@@ -119,56 +72,9 @@ if (!isset($_SESSION["nombre"])) {
 
     //Función que se ejecuta al inicio
     function init() {
-      mostrarform(false);
       listar();
-
-      $("#formulario").on("submit", function (e) {
-        guardaryeditar(e);
-      })
-
-      //Cargamos los items al select categoria
-      $.post("/ajax/articulo.php?op=selectCategoria", function (r) {
-        $("#idcategoria").html(r);
-        $('#idcategoria').selectpicker('refresh');
-
-      });
-      $("#imagenmuestra").hide();
       $('#mAlmacen').addClass("treeview active");
       $('#lArticulos').addClass("active");
-    }
-
-    //Función limpiar
-    function limpiar() {
-      $("#codigo").val("");
-      $("#nombre").val("");
-      $("#descripcion").val("");
-      $("#stock").val("");
-      $("#imagenmuestra").attr("src", "");
-      $("#imagenactual").val("");
-      $("#print").hide();
-      $("#idarticulo").val("");
-    }
-
-    //Función mostrar formulario
-    function mostrarform(flag) {
-      limpiar();
-      if (flag) {
-        $("#listadoregistros").hide();
-        $("#formularioregistros").show();
-        $("#btnGuardar").prop("disabled", false);
-        $("#btnagregar").hide();
-      }
-      else {
-        $("#listadoregistros").show();
-        $("#formularioregistros").hide();
-        $("#btnagregar").show();
-      }
-    }
-
-    //Función cancelarform
-    function cancelarform() {
-      limpiar();
-      mostrarform(false);
     }
 
     //Función Listar
@@ -208,29 +114,6 @@ if (!isset($_SESSION["nombre"])) {
           "iDisplayLength": 5,//Paginación
           "order": [[0, "desc"]]//Ordenar (columna,orden)
         }).DataTable();
-    }
-    //Función para guardar o editar
-
-    function guardaryeditar(e) {
-      e.preventDefault(); //No se activará la acción predeterminada del evento
-      $("#btnGuardar").prop("disabled", true);
-      var formData = new FormData($("#formulario")[0]);
-
-      $.ajax({
-        url: "/ajax/articulo.php?op=guardaryeditar",
-        type: "POST",
-        data: formData,
-        contentType: false,
-        processData: false,
-
-        success: function (datos) {
-          bootbox.alert(datos);
-          mostrarform(false);
-          tabla.ajax.reload();
-        }
-
-      });
-      limpiar();
     }
 
     function mostrar(idarticulo) {
@@ -275,18 +158,6 @@ if (!isset($_SESSION["nombre"])) {
           });
         }
       })
-    }
-
-    //función para generar el código de barras
-    function generarbarcode() {
-      codigo = $("#codigo").val();
-      JsBarcode("#barcode", codigo);
-      $("#print").show();
-    }
-
-    //Función para imprimir el Código de barras
-    function imprimir() {
-      $("#print").printArea();
     }
 
     init();
